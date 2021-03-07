da1dbb3388cabd65fc26add78d2ae57ba277b94a
Nektarios <39172759+Nektar24@users.noreply.github.com>
2021-03-07 23:31:27 +0200

    changed indentation

--- commands/company-all.js
+++ commands/company-all.js
@@ -8,11 +8,11 @@ module.exports = {
 	name: 'company-all',
 	description: "admin command , it's like posting `/company` 12 times",
 	permissions : 1,
-    execute(message,args) {
-        let fakeargs = ['/company']
+    	execute(message,args) {
+        	let fakeargs = ['/company']
 		for (company in companies){
-            fakeargs[1] = company;
-            company_command.execute(message,fakeargs);
-        }
+            		fakeargs[1] = company;
+            		company_command.execute(message,fakeargs);
+        	}
 	},
 };
